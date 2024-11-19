699dbf3950945ec3f4665236c5ffa308c64031b8
Thangwin2004 <161445444+Thangwin2004@users.noreply.github.com>
2024-11-19 21:28:56 +0700

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void hi(){
+        Console.WriteLine("Enter your name: ");
+
+        string yourName = Console.ReadLine();
+
+        Console.WriteLine("Hello: " + yourName);
+    }
+}
